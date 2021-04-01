f480f5819f79eb0efe3a7c0b472f76ab18478380
aria <aryashahbazi@gmail.com>
2021-04-02 01:10:23 +0430

    XO
    Xo game qith cmake

---
+++ Build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: G:/C++/AW/XO Game/XO-Game
+# Build directory: G:/C++/AW/XO Game/XO-Game/Build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("tests")
---
+++ Build/tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: G:/C++/AW/XO Game/XO-Game/tests
+# Build directory: G:/C++/AW/XO Game/XO-Game/Build/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(myLibrary.unit_tests "G:/C++/AW/XO Game/XO-Game/Build/unit_tests.exe")
+set_tests_properties(myLibrary.unit_tests PROPERTIES  _BACKTRACE_TRIPLES "G:/C++/AW/XO Game/XO-Game/tests/CMakeLists.txt;22;add_test;G:/C++/AW/XO Game/XO-Game/tests/CMakeLists.txt;0;")
